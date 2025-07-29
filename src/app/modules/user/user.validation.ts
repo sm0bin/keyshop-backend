cb3ad02aebea7ceaaa8cc35c8c74c9a32bd6a518
sm0bin <s1810677110@ru.ac.bd>
2025-07-29 20:32:42 +0600

    user validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 
 export const createUserSchema = z.object({
   body: z.object({
-    email: z.string().email("Invalid email address"),
+    email: z.email("Invalid email address"),
     name: z.string().min(2, "Name must be at least 2 characters long"),
     password: z.string().min(8, "Password must be at least 8 characters long"),
-    role: z.enum(["superAdmin", "admin", "user"]).default("user"),
+    role: z.enum(["superAdmin", "admin", "user"]).default("user").optional(),
   }),
 });
 
